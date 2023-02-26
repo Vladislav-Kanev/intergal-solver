d45b8947f338588b735d3d9b25ad66db03363cff
HabKaffee <neknaum1@gmail.com>
2023-02-26 19:43:14 +0300

    add script for calculation of quad_weight and quad_point

---
+++ legandre.py
@@ -0,0 +1,8 @@
+import numpy as np
+from numpy.polynomial.legendre import leggauss
+
+n = 2 # degree of Legendre polynomial
+quad_points, quad_weights = leggauss(n)
+
+print(f'qp = {", ".join([str(a) for a in quad_points.tolist()])}')
+print(f'qw = {", ".join([str(a) for a in quad_weights.tolist()])}')
